9ba7de0528c215400a775051261e7c1be98abbaf
Rebeca <rebeca.as1994@gmail.com>
2022-07-03 18:35:04 +0200

    Resolviendo errores

--- ProyectoBackEndIndividual/routes/users.js
+++ ProyectoBackEndIndividual/routes/users.js
@@ -7,7 +7,7 @@ const upload = require('../middlewares/multer');
 router.post('/', upload.single("avatar"), UserController.create)
 router.post('/login', UserController.login)
 router.get('/confirm/:emailToken', UserController.confirm)
-router.put('/logout',authentication, UserController.logout)
+router.delete('/logout',authentication, UserController.logout)
 router.delete('/id/:_id', authentication, UserController.delete)
 router.delete('/admin/id/:_id', authentication, isAdmin, UserController.deleteByAdmin)
 router.get('/', UserController.getAll)
