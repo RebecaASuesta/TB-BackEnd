7f08142d2c56dbfb04be021807a831cc6e441e0d
Rebeca <rebeca.as1994@gmail.com>
2022-07-12 17:07:33 +0200

    Paginación deshabilitada

--- ProyectoBackEndIndividual/controllers/PostController.js
+++ ProyectoBackEndIndividual/controllers/PostController.js
@@ -53,11 +53,11 @@ const PostController = {
 
     async getAll(req, res) {
         try {
-            const { page = 1, limit = 10 } = req.query;
+            // const { page = 1, limit = 10 } = req.query;
             const posts = await Post.find()
                 .populate("commentIds")
-                .limit(limit * 1)
-                .skip((page - 1) * limit);
+                // .limit(limit * 1)
+                // .skip((page - 1) * limit);
             res.send(posts)
         } catch (error) {
             console.error(error)
